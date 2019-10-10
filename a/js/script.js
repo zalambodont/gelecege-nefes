9d3da7ac629ce563b4f96b998f4a68188b20c180
Meriç Kara <selam@merickara.net>
2019-10-10 13:33:16 +0300

    Tiny fix for jQuery

--- a/js/script.js
+++ a/js/script.js
@@ -1,73 +1,94 @@
-/*! SVG Türkiye Haritası | MIT Lisans | dnomak.com */
+$(document).ready(function () {
 
-function init() {
-  const element = document.querySelector('#breath-map');
+  $(document).on("click", "#breath-map path", function () {
+    const parent = $(this).parent();
+      const className = $(parent).attr('class');
+      const target = $(parent).attr('data-target');
+      const completed = $(parent).attr('data-completed');
+      const name = $(parent).attr('data-name');
+      const percent = $(parent).attr('data-percent');
+      const id = $(parent).attr('data-id');
+      const valuekey = parseInt($(parent).attr('data-valuekey'), 10);
+      const url = 'a/img/provinces/' + id + '.svg'
 
+      $('#province-detail .turkey').html(name)
+      $('#province-detail .target').html(target)
+      $('#province-detail .completed').html(completed)
+      $('#province-detail .percent strong').html(percent)
+      $('#province-detail .target').html(target)
+      $('#province-detail figure').attr('class', className);
+      $('#province-detail figure').html('')
+      $('#province-detail figure').prepend('<img src="' + url + '" />')
+      var $img = jQuery('#detail figure img');
 
-  element.addEventListener(
-    'click',
-    function (event) {
-      if (event.target.tagName === 'path') {
-        const parent = event.target.parentNode;
-        const className= parent.getAttribute('class');
-        const target= parent.getAttribute('data-target');
-        const completed= parent.getAttribute('data-completed');
-        const name= parent.getAttribute('data-name');
-        const percent= parent.getAttribute('data-percent');
-        const id = parent.getAttribute('data-id');
-        const valuekey = parseInt(parent.getAttribute('data-valuekey'), 10);
-        const url = 'a/img/provinces/'+id+'.svg'
+      jQuery.get(url, function (data) {
+        var $svg = jQuery(data).find('svg');
+        $svg = $svg.removeAttr('xmlns:a');
+        $img.replaceWith($svg);
+        if (valuekey === 100) {
+          $('#province-detail figure').append('<img src="a/img/tree.svg" class="tree" />')
+        }
+      }, 'xml');
+      if ($('.initial').length) {
         $('.initial').removeClass('initial')
-        $('#detail .turkey').html(name)
-        $('#detail .target').html(target)
-        $('#detail .completed').html(completed)
-        $('#detail .percent strong').html(percent)
-        $('#detail .target').html(target)
-        $('#detail figure').attr('class', className);
-        $('#detail figure').html('')
-        $('#detail figure').prepend('<img src="'+url+'" />')
-        var $img = jQuery('#detail figure img');
-        
-        jQuery.get(url, function(data) {
-          var $svg = jQuery(data).find('svg');
-          $svg = $svg.removeAttr('xmlns:a');
-          $img.replaceWith($svg);
-          if (valuekey === 100){
-            $('#detail figure').append('<img src="a/img/tree.svg" class="tree" />')   
-          }
-      }, 'xml');    
+        console.log('labada')
         setTimeout(() => {
+          $('#top-5').show()
+          $('#province-detail').show()
+          $('#form').hide()
           $('#detail').show()
         }, 300);
+      } else {
+        $('#province-detail').show()
+        $('#form').hide()
+        $('#detail').show()
       }
-    }
-  );
-}
+  });
 
-$(document).ready(function () {
+
   $.get('a/js/data.json',
     function (data, textStatus, jqXHR) {
       const province = data.places
       $.each(province, function (key, value) {
-        $('[data-id='+key+']').attr('class', 'percent-'+value.percent);
-        $('[data-id='+key+']').attr('data-target', value.target)
-        $('[data-id='+key+']').attr('data-completed', value.completed)
-        $('[data-id='+key+']').attr('data-percent', '%'+value.percent)
-        $('[data-id='+key+']').attr('data-valuekey', value.percent)
+        $('[data-id=' + key + ']').attr('class', 'percent-' + value.percent);
+        $('[data-id=' + key + ']').attr('data-target', value.target)
+        $('[data-id=' + key + ']').attr('data-completed', value.completed)
+        $('[data-id=' + key + ']').attr('data-percent', '%' + value.percent)
+        $('[data-id=' + key + ']').attr('data-valuekey', value.percent)
       });
       $('#target').html(province[100].target)
       $('#completed').html(province[100].completed)
-      $('#turkeypercent').html("%"+province[100].percent)
-      if(province[100].percent !== 100){
+      $('#turkeypercent').html("%" + province[100].percent)
+      if (province[100].percent !== 100) {
         $('#startColor').attr("offset", province[100].percent - 25 + "%")
         $('#endColor').attr("offset", province[100].percent + 5 + "%")
-      }
-      else{
+      } else {
         $('#startColor').attr("offset", "100%")
         $('#endColor').attr("offset", "100%")
       }
     });
-
+  $(document).on("click", "#joinCTA", function () {
+    if ($('.initial').length) {
+      $('.initial').removeClass('initial')
+      console.log('labada')
+      setTimeout(() => {
+        $('#top-5').show()
+        $('#province-detail').hide()
+        $('#form').show()
+        $('#detail').show()
+      }, 300);
+    } else {
+      $('#province-detail').hide()
+      $('#form').show()
+      $('#detail').show()
+    }
+    return false
+  });
+  $(document).on("click", "#join", function () {
+    $('#form').show()
+    $('#province-detail').hide()
+    return false
+  });
 
 
 })
